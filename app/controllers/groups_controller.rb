00c1d07f855d9b49731e191638aaa995c35c1c52
tosh-g <aquilapoeta@gmail.com>
2019-06-20 20:26:28 +0900

    create view of group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,17 @@
 class GroupsController < ApplicationController
+  def new
+  end
+
+  def create
+
+  def edit
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
 end
